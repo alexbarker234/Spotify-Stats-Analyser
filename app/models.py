a02e0c1c8c99fe6ca8e04810c477da837a3317b6
alexbarker234 <alex.barker234@gmail.com>
2023-04-20 12:26:13 +0800

    data processing start

--- app/models.py
+++ app/models.py
@@ -1,21 +1,21 @@
 from app import db
 from datetime import datetime
 
-#class User(db.Model):
-
-# cache some data locally to speed up load times (especially with lyrics)
-class Playlist(db.Model):
+class User(db.Model):
     id = db.Column(db.String(120), primary_key=True)
-    last_cache_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    username = db.Column(db.String(120))
 
-    name = db.Column(db.String(120), nullable=True)
-    owner_id = db.Column(db.String(120))
-    track_count = db.Column(db.Integer)
-    image_url = db.Column(db.String(), nullable=True)
+class Listen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    track_id = db.Column(db.String(120), db.ForeignKey('track.id'))
+    user_id = db.Column(db.String(120), db.ForeignKey('user.id'))
+
+    end_time = db.Column(db.DateTime, index=True)
+    ms_listened = db.Column(db.Integer)
 
 class Track(db.Model):
     id = db.Column(db.String(120), primary_key=True)
-    last_cache_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     name = db.Column(db.String(120))
     preview_url = db.Column(db.String(), nullable=True)
@@ -23,28 +23,12 @@ class Track(db.Model):
 
 class Artist(db.Model):
     id = db.Column(db.String(120), primary_key=True)
-    last_cache_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     name = db.Column(db.String(120), nullable=True)
     image_url = db.Column(db.String(), nullable=True)
 
 class TrackArtist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    lastCacheDate = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     artist_id = db.Column(db.String(120), db.ForeignKey('artist.id'))
     track_id = db.Column(db.String(120), db.ForeignKey('track.id'))
-
-class TrackLyrics(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    last_cache_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-
-    lyric_count = db.Column(db.Integer)
-    track_id = db.Column(db.String(120)) # loose foreign key 
-
-class Lyric(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    order = db.Column(db.Integer)
-    lyric = db.Column(db.String())
-    track_lyric_id = db.Column(db.String(120), db.ForeignKey('track_lyrics.id')) 
--- app/routes.py
+++ app/routes.py
@@ -19,19 +19,23 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home', userdata={})#, userdata=UserData()
+    # , userdata=UserData()
+    return render_template('index.html', title='Home', userdata={})
+
 
 @app.route('/login')
-def login():    
+def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
+
 @app.route('/logout')
-def logout():    
+def logout():
     for key in list(session.keys()):
         session.pop(key)
     return redirect('/')
+
 
 @app.route('/authorize')
 def authorize():
@@ -42,20 +46,55 @@ def authorize():
     session["token_info"] = token_info
     return redirect("/index")
 
+
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
         f = request.files.get('file')
-        path = os.path.join(basedir, "testdir")
+        json_data: EndSong = json.load(f, object_hook=EndSong.from_json)
+
+        '''path = os.path.join(basedir, "testdir")
         os.makedirs(path, exist_ok=True)
-        f.save(os.path.join(path, f.filename))
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        f.save(os.path.join(path, f.filename))'''
+
+        for endsong in json_data:
+            endsong
+
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
     except Exception as e:
         return "An error occured", 500
 
 
+class EndSong:
+    def __init__(self, ts, username, ms_played, spotify_track_uri, master_metadata_track_name, master_metadata_album_artist_name):
+        self.ts = ts
+        self.username = username
+        self.ms_played = ms_played
+        self.track_id = spotify_track_uri[14:] # take only the ID from the URI eg spotify:track:xxxxxxxxxxxxxxxxxxxxxx -> xxxxxxxxxxxxxxxxxxxxxx
+        self.track_name = master_metadata_track_name
+        self.artist_name = master_metadata_album_artist_name
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def to_json(self):
+        return self.__str__()
+
+    staticmethod
+    def from_json(json_dct):
+        return EndSong(json_dct['label'],
+                       json_dct['username'], 
+                       json_dct['ms_played'],
+                       json_dct['spotify_track_uri'], 
+                       json_dct['master_metadata_track_name'],  
+                       json_dct['master_metadata_album_artist_name'])
 
 # Checks to see if token is valid and gets a new token if not
+
+
 def get_token():
     token_valid = False
     token_info = session.get("token_info", {})
@@ -72,17 +111,20 @@ def get_token():
     # Refreshing token if it has expired
     if (is_token_expired):
         sp_oauth = create_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+        token_info = sp_oauth.refresh_access_token(
+            session.get('token_info').get('refresh_token'))
 
     token_valid = True
     return token_info, token_valid
 
+
 def create_spotify_oauth():
     return SpotifyOAuth(
-            client_id = Config.SPOTIPY_CLIENT_ID,
-            client_secret = Config.SPOTIPY_CLIENT_SECRET,
-            redirect_uri = url_for('authorize', _external=True),
-            scope = "user-library-read user-top-read playlist-read-private")
+        client_id=Config.SPOTIPY_CLIENT_ID,
+        client_secret=Config.SPOTIPY_CLIENT_SECRET,
+        redirect_uri=url_for('authorize', _external=True),
+        scope="user-library-read user-top-read playlist-read-private")
+
 
 class UserData:
     def __init__(self):
@@ -90,8 +132,10 @@ class UserData:
         session.modified = True
         print("Getting user data")
         if self.authorized:
-            sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+            sp = spotipy.Spotify(auth=session.get(
+                'token_info').get('access_token'))
             payload = sp.me()
-            self.username = payload['display_name']     
-            self.image_url = payload['images'][0]['url'] if len(payload['images']) > 0 else None
+            self.username = payload['display_name']
+            self.image_url = payload['images'][0]['url'] if len(
+                payload['images']) > 0 else None
             self.id = payload['id']
